8a19dd6c162c8c5208915e2638348a82e8e3f273
Chris Cooper <chris.cooper@abcellera.com>
2023-10-19 13:07:27 -0700

    exercise-7

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,12 +2,19 @@ import React from "react";
 import { Work_Sans, Spline_Sans_Mono } from "next/font/google";
 import clsx from "clsx";
 
-import { LIGHT_TOKENS, DARK_TOKENS, BLOG_TITLE } from "@/constants";
+import {
+  LIGHT_TOKENS,
+  DARK_TOKENS,
+  BLOG_TITLE,
+  COLOR_THEME_COOKIE_NAME,
+} from "@/constants";
 
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import "./styles.css";
 import RespectMotionPreferences from "@/components/RespectMotionPreferences";
+
+import { cookies } from "next/headers";
 
 const mainFont = Work_Sans({
   subsets: ["latin"],
@@ -28,8 +35,8 @@ export const metadata = {
 };
 
 function RootLayout({ children }) {
-  // TODO: Dynamic theme depending on user preference
-  const theme = "light";
+  const savedTheme = cookies().get(COLOR_THEME_COOKIE_NAME);
+  const theme = savedTheme?.value || "light";
 
   return (
     <RespectMotionPreferences>
@@ -40,7 +47,7 @@ function RootLayout({ children }) {
         style={theme === "light" ? LIGHT_TOKENS : DARK_TOKENS}
       >
         <body>
-          <Header theme={theme} />
+          <Header initialTheme={theme} />
           <main>{children}</main>
           <Footer />
         </body>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,44 @@
-import React from 'react';
-import clsx from 'clsx';
-import { Rss, Sun, Moon } from 'react-feather';
+"use client";
+import React from "react";
+import clsx from "clsx";
+import { Rss, Sun, Moon } from "react-feather";
 
-import Logo from '@/components/Logo';
-import VisuallyHidden from '@/components/VisuallyHidden';
+import Logo from "@/components/Logo";
+import VisuallyHidden from "@/components/VisuallyHidden";
 
-import styles from './Header.module.css';
+import styles from "./Header.module.css";
 
-function Header({ theme, className, ...delegated }) {
+import Cookie from "js-cookie";
+
+import {
+  COLOR_THEME_COOKIE_NAME,
+  LIGHT_TOKENS,
+  DARK_TOKENS,
+} from "@/constants";
+
+function Header({ initialTheme, className, ...delegated }) {
+  const [theme, setTheme] = React.useState(initialTheme);
+
+  function handleThemeChange() {
+    const nextTheme = theme === "light" ? "dark" : "light";
+    setTheme(nextTheme);
+
+    Cookie.set(COLOR_THEME_COOKIE_NAME, nextTheme, {
+      expires: 1000,
+    });
+
+    const nextTokens = nextTheme === "light" ? LIGHT_TOKENS : DARK_TOKENS;
+    const root = document.documentElement;
+
+    root.setAttribute("data-color-theme", nextTheme);
+
+    for (const [key, value] of Object.entries(nextTokens)) {
+      root.style.setProperty(key, value);
+    }
+  }
+
   return (
-    <header
-      className={clsx(styles.wrapper, className)}
-      {...delegated}
-    >
+    <header className={clsx(styles.wrapper, className)} {...delegated}>
       <Logo />
 
       <div className={styles.actions}>
@@ -21,18 +47,14 @@ function Header({ theme, className, ...delegated }) {
             size="1.5rem"
             style={{
               // Optical alignment
-              transform: 'translate(2px, -2px)',
+              transform: "translate(2px, -2px)",
             }}
           />
-          <VisuallyHidden>
-            View RSS feed
-          </VisuallyHidden>
+          <VisuallyHidden>View RSS feed</VisuallyHidden>
         </button>
-        <button className={styles.action}>
-          <Sun size="1.5rem" />
-          <VisuallyHidden>
-            Toggle dark / light mode
-          </VisuallyHidden>
+        <button className={styles.action} onClick={handleThemeChange}>
+          {theme === "dark" ? <Sun size="1.5rem" /> : <Moon size="1.5rem" />}
+          <VisuallyHidden>Toggle dark / light mode</VisuallyHidden>
         </button>
       </div>
     </header>
